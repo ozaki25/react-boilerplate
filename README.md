b0505892c12e9cb04d5c415135242d36ac783eb7
ozaki25 <fs65ugumori@yahoo.co.jp>
2018-06-25 19:03:31 +0900

    complete

---
+++ README.md
@@ -0,0 +1,25 @@
+# react_boilerplate
+
+## boilerplateを作るまでにやったっこと
+
+```bash
+npm init -y
+npm i react react-dom
+npm i -D @babel/preset-env @babel/preset-react @babel/core babel-core babel-loader webpack webpack-cli webpack-dev-server
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+```
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require("path");
+const webpack = require("webpack");
+const DEV_PORT = process.env.PORT || 3000;
+
+module.exports = {
+  entry: {
+    app: ["./src/index.js"]
+  },
+  output: {
+    path: __dirname + "/public",
+    filename: "bundle.js"
+  },
+  devServer: {
+    contentBase: "public/",
+    port: DEV_PORT
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: "babel-loader",
+        exclude: /node_modules/
+      }
+    ]
+  },
+  plugins: []
+};
